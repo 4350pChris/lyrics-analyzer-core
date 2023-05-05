e57da48ab7e6c88e38b19e06781fe9c67f8a2787
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-05 14:08:07 +0200

    switch to just returning values in stats entity, add average length and top X words
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- src/domain/entities/Stats.ts
+++ src/domain/entities/Stats.ts
@@ -4,12 +4,25 @@ export class Stats extends BaseEntity {
   constructor(
     public artistId: number,
     public wordList: Record<string, number>,
-    public uniqueWords?: number,
   ) {
     super();
   }
 
-  public calculateUniqueWords(): void {
-    this.uniqueWords = Object.keys(this.wordList).length;
+  public calculateUniqueWords(): number {
+    return Object.keys(this.wordList).length;
   }
+
+  public calculateAverageLengthOfWords(): number {
+    const totalLength = Object.entries(this.wordList)
+      .reduce((acc, [word, count]) => acc + word.length * count, 0);
+    const totalWords = Object.values(this.wordList).reduce((acc, count) => acc + count, 0)
+    return totalLength / totalWords;
+  }
+
+  public getTopXWords(x: number): [string, number][] {
+    return Object.entries(this.wordList)
+      .sort(([, a], [, b]) => b - a)
+      .slice(0, x);
+  }
+
 }
--- test/domain/entities/Stats.test.ts
+++ test/domain/entities/Stats.test.ts
@@ -5,6 +5,25 @@ const makeStats = (wordList: Record<string, number>) => new Stats( 1, wordList);
 
 test("Calculate unique words", (t) => {
   const stats = makeStats({ a: 1, b: 2, c: 3 });
-  stats.calculateUniqueWords()
-  t.is(stats.uniqueWords, 3);
+  const uniques = stats.calculateUniqueWords()
+  t.is(uniques, 3);
 })
+
+test("Get top X words", (t) => {
+  const stats = makeStats({ a: 1, b: 2, c: 3, d: 1 });
+  const topX = stats.getTopXWords(2)
+  t.deepEqual(topX, [['c', 3], ['b', 2]]);
+});
+
+test("Get average length of words", (t) => {
+  const stats = makeStats({ a: 1, b: 2, c: 3 });
+  const averageLength = stats.calculateAverageLengthOfWords();
+  t.is(averageLength, 1);
+});
+
+test("Get average length of words, make sure weights are taken into account", (t) => {
+  // (1 * 1 + 2 * 2 + 3 * 3) / (1 + 2 + 3) = 14 / 6
+  const stats = makeStats({ a: 1, aa: 2, aaa: 3 });
+  const averageLength = stats.calculateAverageLengthOfWords();
+  t.is(averageLength, 14 / 6);
+});
