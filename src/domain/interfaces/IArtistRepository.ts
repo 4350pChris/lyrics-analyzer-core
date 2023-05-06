1bca1c6306a35f961ea0e6c02e21f4342aa3cf09
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-06 16:35:38 +0200

    actually get rid of service in domain layer as it only duplicates logic artist domain entity should be fetched from the repository directly and can then be used to calculate stats
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- src/domain/interfaces/IArtistRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Artist } from '../entities/Artist';
-
-export interface IArtistRepository {
-  getArtistById(id: number): Promise<Artist>;
-}
--- src/domain/interfaces/IArtistService.ts
+++
@@ -1,5 +0,0 @@
-import { Stats } from '../entities/Stats';
-
-export interface IArtistService {
-  getStatsForArtist(artistId: number): Promise<Stats>;
-}
