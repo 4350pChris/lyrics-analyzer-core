cbd5b3feb055a00abb59028d960a8e423d98ccca
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-06 14:05:59 +0200

    add test and method for word list in Song entity
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- src/domain/entities/Song.ts
+++ src/domain/entities/Song.ts
@@ -9,4 +9,13 @@ export class Song extends BaseEntity {
   ) {
     super();
   }
+
+  public getWordList(): Record<string, number> {
+    return this.text.split(/\s+/)
+      .reduce((acc, word) => {
+        const count = acc[word] || 0;
+        acc[word] = count + 1;
+        return acc;
+      }, {} as Record<string, number>);
+  }
 }
---
+++ test/domain/entities/Song.test.ts
@@ -0,0 +1,16 @@
+import test from "ava";
+import { Song } from "../../../src/domain/entities/Song.js";
+
+const makeSong = (name: string, text: string) => new Song(1, name, text);
+
+test("Create song", (t) => {
+  const song = makeSong("name", "text");
+  t.is(song.name, "name");
+  t.is(song.text, "text");
+});
+
+test("Get word list", (t) => {
+  const song = makeSong("name", "some text goes here");
+  const wordList = song.getWordList();
+  t.deepEqual(wordList, { text: 1, goes: 1, here: 1, some: 1 });
+});
