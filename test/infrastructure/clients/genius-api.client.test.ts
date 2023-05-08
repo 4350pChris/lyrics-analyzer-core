4d7a75df626bcdf87690ae64352f23dd9e21ce9a
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-08 21:23:07 +0200

    add genius client integration test, complete genius service tests
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

---
+++ test/infrastructure/clients/genius-api.client.test.ts
@@ -0,0 +1,46 @@
+import process from 'node:process';
+// Easy setup for local access token
+// eslint-disable-next-line import/no-unassigned-import
+import 'dotenv/config.js';
+import test from 'ava';
+import {GeniusApiClient} from '@/infrastructure/clients/genius-api.client';
+import {type GeniusApi} from '@/infrastructure/interfaces/genius-api.interface';
+
+let client: GeniusApi;
+
+test.beforeEach('Setup client', () => {
+	const token = process.env.GENIUS_ACCESS_TOKEN;
+	if (!token) {
+		throw new Error('Genius access token has to be set for client integration test to work');
+	}
+
+	client = new GeniusApiClient(
+		'https://api.genius.com',
+		token,
+	);
+});
+
+test('get artist by id', async t => {
+	const {response} = await client.getArtist(150);
+	const {artist} = response;
+
+	t.truthy(artist);
+});
+
+test('search artists', async t => {
+	const {response} = await client.search('MF DOOM');
+
+	t.not(response.hits.length, 0);
+});
+
+test('get songs for artist', async t => {
+	const {response} = await client.getSongsForArtist(150, 1);
+
+	t.not(response.songs.length, 0);
+});
+
+test('get song', async t => {
+	const html = await client.getSong('https://genius.com/Madvillain-accordion-lyrics');
+
+	t.regex(html, /^<html/);
+});
--- test/infrastructure/services/genius.service.test.ts
+++ test/infrastructure/services/genius.service.test.ts
@@ -5,6 +5,8 @@ import type {GeniusApi} from '@/infrastructure/interfaces/genius-api.interface.j
 import {type SearchResponse} from '@/infrastructure/dtos/search-response.dto.js';
 import {type GeniusSongDto} from '@/infrastructure/dtos/genius-song.dto';
 import {type LyricsParser} from '@/infrastructure/interfaces/lyrics-parser.interface';
+import {type ArtistDetailResponse} from '@/infrastructure/dtos/artist-detail-response.dto';
+import {type ArtistSongsResponse} from '@/infrastructure/dtos/artist-songs-response.dto';
 
 const getMockSong: () => Omit<GeniusSongDto, 'primary_artist'> = () => ({
 	id: 3810,
@@ -12,99 +14,114 @@ const getMockSong: () => Omit<GeniusSongDto, 'primary_artist'> = () => ({
 	url: 'https://genius.com/Madvillain-accordion-lyrics',
 });
 
-test('Search artists should consolidate artists from songs', async t => {
-	const mockClient: GeniusApi = {
-		getArtist(artistId) {
-			throw new Error('Not implemented');
-		},
-		async search(query) {
-			return {
-				meta: {
-					status: 200,
-				},
-				response: {
-					hits: [
-						{
-							result: {
-								...getMockSong(),
-								primary_artist: {
-									id: 150,
-									name: 'Madvillain',
-								},
-							},
-						},
-						{
-							result: {
-								...getMockSong(),
-								primary_artist: {
-									id: 151,
-									name: 'MF DOOM',
-								},
-							},
-						},
-						{
-							result: {
-								...getMockSong(),
-								primary_artist: {
-									id: 150,
-									name: 'Madvillain',
-								},
-							},
-						},
-					],
-				},
-			} satisfies SearchResponse;
-		},
-		async getSongsForArtist() {
-			throw new Error('not implemented');
-		},
-		async getSong(url) {
-			throw new Error('not implemented');
-		},
+const createMockClient: () => GeniusApi = () => ({
+	async search(query: string): Promise<SearchResponse> {
+		throw new Error('Function not implemented.');
+	},
+	async getSongsForArtist(artistId: number, page: number): Promise<ArtistSongsResponse> {
+		throw new Error('Function not implemented.');
+	},
+	async getSong(url: string): Promise<string> {
+		throw new Error('Function not implemented.');
+	},
+	async getArtist(artistId: number): Promise<ArtistDetailResponse> {
+		throw new Error('Function not implemented.');
+	},
+});
+
+test('get artist should return artist from response', async t => {
+	const artist = {
+		id: 151,
+		name: 'MF DOOM',
+		description: 'ALL CAPS',
+		image_url: 'url',
 	};
+	const mockClient = createMockClient();
+	mockClient.getArtist = async artistId => ({
+		meta: {
+			status: 200,
+		},
+		response: {
+			artist,
+		},
+	});
+	const geniusService = new GeniusService(mockClient, {} as LyricsParser);
+
+	const responseArtist = await geniusService.getArtist(150);
+
+	t.like(responseArtist, artist);
+});
+
+test('Search artists should consolidate artists from songs', async t => {
+	const mockClient = createMockClient();
+	mockClient.search = async query => ({
+		meta: {
+			status: 200,
+		},
+		response: {
+			hits: [
+				{
+					result: {
+						...getMockSong(),
+						primary_artist: {
+							id: 150,
+							name: 'Madvillain',
+						},
+					},
+				},
+				{
+					result: {
+						...getMockSong(),
+						primary_artist: {
+							id: 151,
+							name: 'MF DOOM',
+						},
+					},
+				},
+				{
+					result: {
+						...getMockSong(),
+						primary_artist: {
+							id: 150,
+							name: 'Madvillain',
+						},
+					},
+				},
+			],
+		},
+	} satisfies SearchResponse);
 	const geniusService = new GeniusService(mockClient, {} as LyricsParser);
 	const artists = await geniusService.searchArtists('MF DOOM');
 	t.is(artists.length, 2);
 });
 
-test('Get paginated songs should return a list of songs witht their lyrics', async t => {
-	const mockClient: GeniusApi = {
-		getArtist(artistId) {
-			throw new Error('Not implemented');
+test('Get paginated songs should return a list of songs with their lyrics', async t => {
+	const mockClient = createMockClient();
+	mockClient.getSong = async url => 'text';
+	mockClient.getSongsForArtist = async (artistId, page) => ({
+		meta: {
+			status: 200,
 		},
-		async search() {
-			throw new Error('not implemented');
-		},
-		async getSong(url) {
-			return 'text';
-		},
-		async getSongsForArtist() {
-			return {
-				meta: {
-					status: 200,
+		response: {
+			next_page: undefined,
+			songs: [
+				{
+					...getMockSong(),
+					primary_artist: {
+						id: 150,
+						name: 'Madvillain',
+					},
 				},
-				response: {
-					next_page: undefined,
-					songs: [
-						{
-							...getMockSong(),
-							primary_artist: {
-								id: 150,
-								name: 'Madvillain',
-							},
-						},
-						{
-							...getMockSong(),
-							primary_artist: {
-								id: 151,
-								name: 'MF DOOM',
-							},
-						},
-					],
+				{
+					...getMockSong(),
+					primary_artist: {
+						id: 151,
+						name: 'MF DOOM',
+					},
 				},
-			};
+			],
 		},
-	};
+	});
 
 	const geniusService = new GeniusService(mockClient, {
 		parse(html: string) {
