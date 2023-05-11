71a66a9909478ca4e3e125f135e5134504499861
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-11 19:52:40 +0200

    add analyze lyrics use case
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

---
+++ src/application/usecases/analyze-lyrics/analyze-lyrics.usecase.ts
@@ -0,0 +1,27 @@
+import {type ParsedSongsDto} from '@/application/dtos/parsed-songs.dto';
+import {type UseCase} from '@/application/interfaces/usecase';
+import {type ArtistAggregate} from '@/domain/entities/artist.aggregate';
+import {type ArtistRepository} from '@/domain/interfaces/artist-repository.interface';
+
+export class AnalyzeLyrics implements UseCase {
+	constructor(
+		private readonly artistRepository: ArtistRepository,
+	) {}
+
+	async execute({artistId, songs}: ParsedSongsDto): Promise<void> {
+		let artist: ArtistAggregate;
+		try {
+			artist = await this.artistRepository.getById(Number.parseInt(artistId));
+		} catch (error) {
+			throw new Error('Artist not found', {cause: error});
+		}
+
+		for (const song of songs) {
+			artist.addSong(song.id, song.title, song.text, song.url);
+		}
+
+		artist.calculateStats();
+
+		await this.artistRepository.save(artist);
+	}
+}
---
+++ test/application/usecases/analyze-lyrics/analyze-lyrics.usecase.test.ts
@@ -0,0 +1,28 @@
+import test from 'ava';
+import td from 'testdouble';
+import {AnalyzeLyrics} from '@/application/usecases/analyze-lyrics/analyze-lyrics.usecase';
+import {type ArtistRepository} from '@/domain/interfaces/artist-repository.interface';
+
+const setupMocks = () => ({
+	artistRepository: td.object<ArtistRepository>(),
+});
+
+test('should call artistRepository.getById with correct params', async t => {
+	const {artistRepository} = setupMocks();
+	const usecase = new AnalyzeLyrics(artistRepository);
+
+	td.when(artistRepository.getById(1)).thenResolve(td.object());
+
+	await usecase.execute({artistId: '1', songs: []});
+
+	t.is(td.explain(artistRepository.getById).callCount, 1);
+});
+
+test('should throw error if artist is not found', async t => {
+	const {artistRepository} = setupMocks();
+	const usecase = new AnalyzeLyrics(artistRepository);
+
+	td.when(artistRepository.getById(1)).thenReject(new Error('Internal error'));
+
+	await t.throwsAsync(usecase.execute({artistId: '1', songs: []}), {message: 'Artist not found'});
+});
