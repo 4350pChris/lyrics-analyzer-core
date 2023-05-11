5be40b5266faf2b1212d38f9c11afea75ac5ab92
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-11 14:15:42 +0200

    fix sqs queue service test timeouts
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- test/infrastructure/services/sqs-queue.service.test.ts
+++ test/infrastructure/services/sqs-queue.service.test.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import test from 'ava';
 import td from 'testdouble';
-import type {SQS} from 'aws-sdk';
+import {SQS} from 'aws-sdk';
 import {SqsQueueService} from '@/infrastructure/services/sqs-queue.service';
 
 const setupMocks = () => ({
-	sqs: td.object<SQS>(),
+	sqs: td.object(new SQS()),
 	queueUrl: 'https://sqs.us-east-1.amazonaws.com/123456789012/queue-name',
 });
 
@@ -13,14 +12,15 @@ test('publish', async t => {
 	const {sqs, queueUrl} = setupMocks();
 	const queueService = new SqsQueueService(sqs, queueUrl);
 	const message = 'message';
-	const expectedParameters = {
-		MessageBody: message,
-		QueueUrl: queueUrl,
-	};
 
-	td.when(sqs.sendMessage(expectedParameters)).thenReturn(td.object());
+	const innerPromise = td.func();
+	td.when(innerPromise()).thenResolve({});
+
+	td.when(sqs.sendMessage(td.matchers.anything() as SQS.SendMessageRequest)).thenReturn({
+		promise: innerPromise as () => Promise<any>,
+	});
+
 	await queueService.publish(message);
 
-	t.is(td.explain(sqs.sendMessage).callCount, 1);
-	t.deepEqual(td.explain(sqs.sendMessage).calls[0].args[0], expectedParameters);
+	t.is(td.explain(innerPromise).callCount, 1);
 });
