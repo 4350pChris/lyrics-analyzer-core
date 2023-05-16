da05a8cb54b0fda6cdaf3c11748ba507c9409d0d
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-17 00:03:07 +0200

    remove default files
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ArtistSearch from '../components/search/ArtistSearch.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ArtistSearch />
   </main>
 </template>
