1ef894f3f611face98d754e4b90ba7fa5523e918
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-17 14:37:22 +0200

    add list artists api call to frontend
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- frontend/src/api/artists.ts
+++ frontend/src/api/artists.ts
@@ -1,16 +1,34 @@
-const makeUrl = (path: string) => new URL(`/artists${path}`, import.meta.env.VITE_API_BASE_URL)
-
 export type ArtistSearchResult = {
   id: number
   name: string
   imageUrl?: string
 }
 
-export async function searchArtists() {
-  const url = makeUrl('/search')
-  // TODO: validation?
-  const result = await fetch(url)
-  const data = (await result.json()) as ArtistSearchResult[]
+export type ArtistListResult = {
+  artists: {
+    id: number
+    name: string
+    description: string
+    imageUrl?: string
+  }
+}
 
-  return data
+const makeUrl = (path: string) => new URL(`/artists${path}`, import.meta.env.VITE_API_BASE_URL)
+
+const fetchGet = async <T>(path: string) => {
+  const url = makeUrl(path)
+  const result = await fetch(url)
+  const data = await result.json()
+
+  if (!result.ok) throw new Error(data.message)
+
+  return data as T
+}
+
+export const searchArtists = () => fetchGet<ArtistSearchResult>('/search')
+
+export const listArtists = async () => {
+  const data = await fetchGet<ArtistListResult>('/')
+
+  return data.artists
 }
