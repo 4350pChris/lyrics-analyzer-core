91503ab7a3f766b349b4fbf89c0f8dd5999fc029
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-17 02:29:59 +0200

    extend env.d.ts with imports
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -5,3 +5,8 @@ import type { AttributifyAttributes } from '@unocss/preset-attributify'
 declare module '@vue/runtime-dom' {
   interface HTMLAttributes extends AttributifyAttributes {}
 }
+
+// extend import.meta.env
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+}
