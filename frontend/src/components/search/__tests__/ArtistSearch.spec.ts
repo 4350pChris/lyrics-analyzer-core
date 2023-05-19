153cae8b0bf97edf0293927163f365add3634d88
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-05-19 03:41:13 +0200

    add test for model value update event
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- frontend/src/components/search/__tests__/ArtistSearch.spec.ts
+++ frontend/src/components/search/__tests__/ArtistSearch.spec.ts
@@ -1,8 +1,6 @@
 import { mount } from '@vue/test-utils'
-import { searchArtists } from '@/api/artists'
+import * as artistsExports from '@/api/artists'
 import ArtistSearch from '../ArtistSearch.vue'
-
-const mountComponent = () => mount(ArtistSearch)
 
 describe('ArtistSearch', () => {
   afterEach(() => {
@@ -10,21 +8,19 @@ describe('ArtistSearch', () => {
   })
 
   it('renders properly', () => {
-    const wrapper = mountComponent()
+    const wrapper = mount(ArtistSearch)
     expect(wrapper).toBeTruthy()
   })
 
   it('contains a search input', () => {
-    const wrapper = mountComponent()
+    const wrapper = mount(ArtistSearch)
     expect(wrapper.find('input').exists()).toBe(true)
   })
 
   it('Calls search API on user input, debouncing by 250 ms', async () => {
-    const wrapper = mountComponent()
+    const wrapper = mount(ArtistSearch)
 
-    vi.mock('@/api/artists', () => ({
-      searchArtists: vi.fn()
-    }))
+    const spy = vi.spyOn(artistsExports, 'searchArtists').mockResolvedValue([])
 
     const input = wrapper.get('input')
 
@@ -33,11 +29,40 @@ describe('ArtistSearch', () => {
     await input.setValue('query')
     await input.trigger('change')
 
-    expect(searchArtists).not.toHaveBeenCalled()
+    expect(spy).not.toHaveBeenCalled()
 
     await new Promise((resolve) => setTimeout(resolve, 250))
 
-    expect(searchArtists).toHaveBeenCalledOnce()
-    expect(searchArtists).toHaveBeenCalledWith('query')
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('query')
+  })
+
+  it('Emits update:modelValue when item is selected', async () => {
+    const wrapper = mount(ArtistSearch)
+
+    const spy = vi.spyOn(artistsExports, 'searchArtists')
+
+    const artist = { id: 1, name: 'MF DOOM' }
+
+    spy.mockResolvedValueOnce([artist])
+
+    const input = wrapper.get('input')
+
+    expect(input).toBeTruthy()
+
+    await input.setValue('query')
+    await input.trigger('change')
+
+    await new Promise((resolve) => setTimeout(resolve, 250))
+
+    const listItem = wrapper.get('[role=option]')
+
+    expect(listItem).toBeTruthy()
+
+    await listItem.trigger('click')
+
+    expect(wrapper.emitted()).toHaveProperty('update:modelValue')
+    expect(wrapper.emitted()['update:modelValue']).toHaveLength(1)
+    expect(wrapper.emitted()['update:modelValue'][0]).toEqual([artist])
   })
 })
